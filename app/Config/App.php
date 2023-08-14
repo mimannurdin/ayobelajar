f7a49b65bcd1c8061747b87fab664045df52bef7
Muhammad Iman Nurdin <68886028+mimannurdin@users.noreply.github.com>
2023-08-14 20:27:48 +0700

    Update base url on App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      * @var string
      */
     // public $baseURL = 'http://localhost:8080/';
-    public $baseURL = 'https://mimannurdin-ayobelajar.netlify.app/';
+    public $baseURL = 'https://mimannurdin.github.io/ayobelajar/';
 
     /**
      * --------------------------------------------------------------------------
